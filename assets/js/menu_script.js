911b846a7a518b091f1a895fa139eb640643063b
Douglas Alves <alves.douglaz@hotmail.com>
2015-07-30 17:30:13 -0300

    Corrigindo o background cinza para a cor certa

--- assets/js/menu_script.js
+++ assets/js/menu_script.js
@@ -1,6 +1,6 @@
 $(window).ready(function() {
     $('.treeview a').on('click', function() {
-        $(this).parent().find('.treeview-menu').toggleClass('active');
+        $(this).parent().toggleClass('active');
         $(this).parent().find('.treeview-menu').slideToggle('fast').toggleClass('show');
     });
 });
